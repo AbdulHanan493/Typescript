e2c8da52f83309fe0ca27d6c6e2809322838e7b6
AbdulHanan493 <hanankhan493@gmail.com>
2023-02-16 02:15:19 +0500

    All

---
+++ all.js
@@ -0,0 +1,12 @@
+var text = "Hello Programmers", text1 = "Folowing are the Addition,Subtraction,Multiplication,Division & Percentge of the given Qusestion";
+console.log(text + "\n" + text1);
+console.log("Addition of the Following\nQuestion:1\n");
+console.log("The Answer is=" + (10 + 5));
+console.log("Subtraction of the Following\nQuestion:2\n");
+console.log("The Answer is=" + (19 - 4));
+console.log("Multiplication of the Following\nQuestion:3\n");
+console.log("The Answer is=" + (3 * 5));
+console.log("Division of the Following\nQuestion:4\n");
+console.log("The Answer is=" + (30 ^ 2));
+console.log("Percentage of the Following\nQuestion:5\n");
+console.log("The Answer is=" + (165 % 1100));
